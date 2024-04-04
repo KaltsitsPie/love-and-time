5f324c9fffc474ed416734bf979a61e7f9f8099b
Yanchen Zhou <beinu2137@gmail.com>
2024-04-04 14:55:37 -0700

    Initial project

---
+++ loveandTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  loveandTime
+//
+//  Created by 周彦辰 on 2024/4/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ loveandTime/loveandTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  loveandTimeApp.swift
+//  loveandTime
+//
+//  Created by 周彦辰 on 2024/4/4.
+//
+
+import SwiftUI
+
+@main
+struct loveandTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
